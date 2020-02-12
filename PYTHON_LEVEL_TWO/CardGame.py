2d91ba8a98005b533c19aa4cda1ef990b3ba00af
danbokhari98 <danbokhari98@gmail.com>
2020-02-12 12:45:01 -0500

    Fixed card game

--- PYTHON_LEVEL_TWO/CardGame.py
+++ PYTHON_LEVEL_TWO/CardGame.py
@@ -1,7 +1,6 @@
-import Random
+from random import shuffle
 SUITE = 'H D S C'.split()
 RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'. split()
-
 # for r in RANKS:
     #for s in SUITE:
         #mycards.append((s,r))
@@ -12,7 +11,7 @@ class Deck():
     """
     def __init__(self):
         print("Creating new Ordered Deck")
-        self.allcards = [(s,r) for s SUITE for r in RANKS]
+        self.allcards = [(s,r) for s in SUITE for r in RANKS]
 
     def shuffle(self):
         print("SHUFFLING")
@@ -28,7 +27,7 @@ class Hand():
     def __str__(self):
         return "Contains {} cards".fornat(len(self.cards))
     def add(self, added_cards):
-        self.cards.extended(added_cards)
+        self.cards.extend(added_cards)
     def remove_card(self):
         return self.cards.pop()
 
@@ -46,9 +45,12 @@ class Player():
 
     def remove_war_cards(self):
         war_cards = []
-        for x in range(3):
-            war_cards.append(self.hand.cards.pop())
-        return war_cards
+        if len(self.hand.cards) < 3:
+            return self.hand.cards
+        else:
+            for x in range(3):
+                war_cards.append(self.hand.cards.pop())
+            return war_cards
 
     def still_has_cards(self):
         """
@@ -67,3 +69,41 @@ comp = Player("Computer", Hand(half1))
 
 name = input("What is your name?")
 user = Player(name, Hand(half2))
+
+
+total_rounds = 0
+war_count = 0
+
+while user.still_has_cards() and comp.still_has_cards():
+    total_rounds += 1
+    print("Time for a new round!")
+    print("Here are the current standing")
+    print(user.name +"has the count:" +str(len(user.hand.cards)))
+    print(comp.name +"has the count:" +str(len(comp.hand.cards)))
+    print("Play a card!")
+    print('\n')
+
+    table_cards = []
+
+    c_card = comp.play_card()
+    p_card = user.play_card()
+
+    table_cards.append(c_card)
+    table_cards.append(p_card)
+
+    if c_card[1] == p_card[1]:
+        war_count += 1
+        print("War!")
+        table_cards.extend(user.remove_war_cards())
+        table_cards.extend(comp.remove_war_cards())
+        if RANKS.index(c_card[1]) < RANKS.index(p_card[1]):
+            user.hand.add(table_cards)
+        else:
+            comp.hand.add(table_cards)
+    else:
+        if RANKS.index(c_card[1]) < RANKS.index(p_card[1]):
+            user.hand.add(table_cards)
+        else:
+            comp.hand.add(table_cards)
+print("Game over, number of rounds:"+str(total_rounds))
+print("A war happened "+str(war_count) +" times")
