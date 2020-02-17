5afc669f3943619a4a933f4a3dac5559230922e8
danbokhari98 <danbokhari98@gmail.com>
2020-02-17 16:00:13 -0500

    Finished Course level 1 of Django, Linking everything together

--- My_Django_Stuff/ProTwo/ProTwo/urls.py
+++ My_Django_Stuff/ProTwo/ProTwo/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from appTwo import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('help/', include('appTwo.urls')),
     path('admin/', admin.site.urls),
 ]
