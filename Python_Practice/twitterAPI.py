b448e94dcd9e4f1b7bc4d3d4bfcb6afa0eaa8494
danbokhari98 <danbokhari98@gmail.com>
2020-02-21 13:27:09 -0500

    Playing with twitter API

---
+++ Python_Practice/twitterAPI.py
@@ -0,0 +1,12 @@
+import twitter
+
+api = twitter.Api(consumer_key='consumer_key',
+    consumer_secret = 'consumer_secret',
+    access_token_key = 'access_token_key',
+    access_token_secret = 'access_token_secret')
+
+#print(api.VerifyCredentials())
+#statuses = api.GetUserTimeline(screen_name='CantBeatDaBEAST')
+#print([s.text for s in statuses])
+status = api.PostUpdate('Tweeting from Python!')
+print(status.text)
